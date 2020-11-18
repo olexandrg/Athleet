2e3a36fe31c08051ec43e4223607c73dc4aae35c
Simeon Florea <simeon.florea2000@gmail.com>
2020-11-17 18:19:20 -0800

    Wrote GET command in ApiService. IMPORTANT: Currently connecting to local API; API not published

--- app/src/main/java/net/azurewebsites/athleet/MainActivity.kt
+++ app/src/main/java/net/azurewebsites/athleet/MainActivity.kt
@@ -2,10 +2,60 @@ package net.azurewebsites.athleet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
+
+object ApiProvider {
+
+    private fun getHttpClient() : OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
+        return httpClient.build()
+    }
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(ApiService.BASE_URL)
+        .client(getHttpClient())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <S> createService(serviceClass: Class<S>?) : S {
+        return retrofit.create(serviceClass)
+    }
+}
+
+interface ApiService {
+
+    companion object {
+        val BASE_URL: String = "https://localhost:44394/api/"
+    }
+
+    @GET("Users")
+    suspend fun getResult(
+        @Query("q") query: String?): Response<SearchResultModel?>
+
+}
+data class SearchResultModel(
+    val UserId: Int?,
+    val FirebaseUID: String?,
+    val UserName: String?,
+    val UserHeadline: String?
+) {}
+
