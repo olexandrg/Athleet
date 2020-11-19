48b7fed3547447ea58059f1ca308072c4d00cd61
Olexandr Goloshchapov <olexandr.goloshchapov@oit.edu>
2020-11-18 19:18:36 -0800

    Deleted SearchResultModel class. Data classes are aggregated in DataClasses.

--- app/src/main/java/net/azurewebsites/athleet/SearchResultModel.kt
+++
@@ -1,9 +0,0 @@
-package net.azurewebsites.athleet
-
-data class SearchResultModel(
-    val UserId: Int?,
-    val UserName: String?,
-    val UserHeadline: String?,
-    val FirebaseUID: String?
-) {}
-
