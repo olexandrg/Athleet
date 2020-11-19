bc593f4b4f5afe21362f5c549decb92b09c9a247
Ryan Rupert <ryan.rupert@oit.edu>
2020-11-19 15:55:30 -0800

    Clean up Main Activity code behind
    This removes a no longer used variable and unnecessary imports.

--- app/src/main/java/net/azurewebsites/athleet/MainActivity.kt
+++ app/src/main/java/net/azurewebsites/athleet/MainActivity.kt
@@ -1,27 +1,14 @@
 package net.azurewebsites.athleet
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.*
-import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.BufferedInputStream
-import java.io.InputStream
-import java.security.KeyStore
-import java.security.cert.CertificateFactory
-import javax.net.ssl.HttpsURLConnection
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManagerFactory
 
 class MainActivity : AppCompatActivity() {
-    private var coroutineJob: Job? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
