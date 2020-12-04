255ded3e4fbe74c3007214b01067a3f786db8ff4
Ryan Rupert <ryan.rupert@oit.edu>
2020-12-04 13:05:58 -0800

    Modify test ApiUnitTests.kt
    This adds code that will randomly generate and append a string to the
    test username. This is done just in case the test is ran by multiple
    people at the same time or a test fails and the test user isn't deleted.

--- app/src/test/java/net/azurewebsites/athleet/ApiUnitTests.kt
+++ app/src/test/java/net/azurewebsites/athleet/ApiUnitTests.kt
@@ -24,8 +24,13 @@ class ApiUnitTests {
 
     @Test
     fun testUser() {
+        //this generates a random string to append to the end of the username so that it is unique when the test is ran
+        //this will prevent issues if multiple people run the test at the same time or the test fails for someone and the user isn't deleted at the end
+        val charPool: List<Char> = ('a'..'z') + ('A'..'Z')
+        val randomString = (1..6).map { i -> kotlin.random.Random.nextInt(0, charPool.size)}
+            .map(charPool::get).joinToString("")
         //declare var's and val's used throughout the whole method
-        val userName = "TestUser"
+        val userName = "TestUser" + randomString
         var userId: Int? = 0
         val api = apiFactory()
 
