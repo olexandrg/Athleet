9ef1f223dce4559112551f34da2f483640f83113
ntout <nathan.tout@oit.edu>
2021-01-26 09:24:31 -0800

    selecting team list item redirects to TeamDashboardFragment

--- app/src/main/java/net/azurewebsites/athleet/Teams/TeamDetailActivity.kt
+++ app/src/main/java/net/azurewebsites/athleet/Teams/TeamDetailActivity.kt
@@ -3,14 +3,15 @@ package net.azurewebsites.athleet.Teams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import net.azurewebsites.athleet.R
+import net.azurewebsites.athleet.databinding.ActivityTeamDetailBinding
 
 class TeamDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        setContentView(R.layout.activity_team_detail)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityTeamDetailBinding>(this, R.layout.activity_team_detail)
 
     }
 }
