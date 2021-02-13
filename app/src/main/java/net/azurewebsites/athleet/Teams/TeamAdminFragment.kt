a1b056948bd24306cf5ebe70f4d805e0468c8bed
Ryan Rupert <ryan.rupert@oit.edu>
2021-02-12 18:40:37 -0800

    Add a is only admin check to leave team button
    This will check if the user is admin if so require that there is another
    admin before user can leave, if the user isn't an admin then can leave
    at any time.

--- app/src/main/java/net/azurewebsites/athleet/Teams/TeamAdminFragment.kt
+++ app/src/main/java/net/azurewebsites/athleet/Teams/TeamAdminFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_team_admin.*
 import net.azurewebsites.athleet.ApiLib.Api
+import net.azurewebsites.athleet.ApiLib.TeamInfo
 import net.azurewebsites.athleet.Dashboard.TEAM_NAME
 import net.azurewebsites.athleet.R
 import retrofit2.Callback
@@ -26,6 +28,47 @@ class TeamAdminFragment : Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentTeamAdminBinding>(inflater,
             R.layout.fragment_team_admin, container, false )
+        val userName = FirebaseAuth.getInstance().currentUser!!.displayName;
+        button_leaveTeam.isEnabled = false
+
+        val api: Api = Api.createSafe("https://testapi.athleetapi.club/api/")
+        FirebaseAuth.getInstance().currentUser?.getIdToken(false)
+            ?.addOnCompleteListener { response ->
+                if (response.isSuccessful) {
+                    val teamName = activity?.intent?.getStringExtra(TEAM_NAME).toString()
+                    val call = api.teamInfo(response.result?.token.toString(), teamName)
+
+                    call.enqueue(object : Callback<TeamInfo> {
+                        override fun onFailure(
+                            call: retrofit2.Call<TeamInfo>,
+                            t: Throwable
+                        ) {
+                            Toast.makeText(activity, "Failed to get team information", Toast.LENGTH_LONG)
+                                .show()
+                        }
+
+                        override fun onResponse(
+                            call: retrofit2.Call<TeamInfo>,
+                            response: retrofit2.Response<TeamInfo>
+                        ) {
+                            var users = response.body()!!.users
+                            var isAdmin: Boolean = false;
+                            var multAdmin: Int = 0;
+                            for(user in users) {
+                                if (user.isAdmin)
+                                    multAdmin++;
+                                if (user.UserName == userName)
+                                    isAdmin = true
+                            }
+                            if((isAdmin && multAdmin > 2) || !isAdmin)
+                                button_leaveTeam.isEnabled = true
+                        }
+                    })
+                } else {
+                    Toast.makeText(activity, "Couldn't get user token", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
 
         binding.buttonDeleteTeam.setOnClickListener {
             deleteTeam()
