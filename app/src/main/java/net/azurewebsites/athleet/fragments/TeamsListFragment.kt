a0cc9c5c999fd8cd9a8681fcbbf09e018c21a4c2
Ryan Rupert <ryan.rupert@oit.edu>
2021-03-06 15:31:21 -0800

    Complete retrieval of teams list and displaying it

--- app/src/main/java/net/azurewebsites/athleet/fragments/TeamsListFragment.kt
+++ app/src/main/java/net/azurewebsites/athleet/fragments/TeamsListFragment.kt
@@ -16,26 +16,59 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_workouts_list.*
+import net.azurewebsites.athleet.ApiLib.Api
+import net.azurewebsites.athleet.ApiLib.team
 import net.azurewebsites.athleet.Dashboard.*
 import net.azurewebsites.athleet.Teams.*
+import net.azurewebsites.athleet.workouts.Workout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TeamsListFragment : Fragment() {
+    private val api = Api.createSafe()
     private val teamsListViewModel by viewModels<TeamsListViewModel> { TeamsListViewModelFactory(requireContext()) }
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var fab: View
+    private lateinit var token: String
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         linearLayoutManager = LinearLayoutManager(activity)
         val teamsAdapter = TeamsListAdapter { team -> adapterOnClick(team) }
+
+        getTeams()
+
         recyclerView_Workout?.layoutManager = linearLayoutManager
         recyclerView_Workout?.adapter = teamsAdapter
         teamsAdapter.submitList(TeamsList(resources))
 
         // Button goes to Create New Team activity.
         fab = requireActivity().findViewById(R.id.fab)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getTeams() {
+        FirebaseAuth.getInstance().currentUser?.getIdToken(false)?.addOnCompleteListener { response ->
+            if(response.isSuccessful) {
+                token = "Bearer " + response.result?.token.toString()
+                val callGetWorkouts = api.listTeams(token)
+
+                callGetWorkouts.enqueue(object: Callback<List<Team>> {
+                    override fun onResponse(call: Call<List<Team>>, response: Response<List<Team>>) {
+                        if(response.isSuccessful) {
+                            teamsListViewModel.insertTeams(response.body()!!.toList())
+                        }
+                    }
+                    override fun onFailure(call: Call<List<Team>>, t: Throwable) {
+                        TODO("Not yet implemented")
+                    }
+                })
+            }
+        }
     }
 
     override fun onCreateView(
@@ -56,8 +89,10 @@ class TeamsListFragment : Fragment() {
         return rootView
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onResume() {
         super.onResume()
+        getTeams()
         fab.setOnClickListener { fabOnClick() }
     }
 
