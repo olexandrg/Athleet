7ecb3323c6edd69f71ef7f1fa84b8acdb19314ad
Olexandr Goloshchapov <olexandr.goloshchapov@oit.edu>
2021-03-10 09:43:16 -0800

    Removed deprecated API classes. Refactored profile page and retrieve workout tests.

--- app/src/main/java/net/azurewebsites/athleet/ApiLib/DataAggregates.kt
+++
@@ -1,9 +0,0 @@
-package net.azurewebsites.athleet.ApiLib
-
-//class User : ArrayList<UserItem>()
-//class Workouts: ArrayList<WorkoutItem>()
-//class UserWorkoutList: ArrayList<UserWorkouts>()
-//class Exercises : ArrayList<ExercisesItem>()
-//class WorkoutExercises : ArrayList<WorkoutExerciseItem>()
-//class ViewUserWorkouts : ArrayList<ViewUserWorkoutsItem>()
-//class ViewExercisesInWorkout: ArrayList<ViewUserExercisesInWorkoutItems>()
--- app/src/main/java/net/azurewebsites/athleet/ApiLib/ViewUserExercisesInWorkoutItems.kt
+++
@@ -1,9 +0,0 @@
-//package net.azurewebsites.athleet.ApiLib
-//
-//data class ViewUserExercisesInWorkoutItems(
-//    val userName: String,
-//    val workoutName: String,
-//    val exerciseName: String,
-//    val description: String,
-//    val defaultReps: Int
-//)
