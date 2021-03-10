efdfb9deee89c38299c6fc71970da7873d5c2c8e
tpaladichuk <taylor.paladichuk@oit.edu>
2021-03-09 21:52:34 -0800

    Finished unit tests for updating a User's info. The checkEditUser test thoroughly ensures that the only the Username and Headline are modified in the process of performing multiple changes.

--- app/src/test/java/net/azurewebsites/athleet/ApiUnitTests.kt
+++ app/src/test/java/net/azurewebsites/athleet/ApiUnitTests.kt
@@ -43,13 +43,57 @@ class ApiUnitTests {
     @Test
     fun checkEditUser() {
         val api = apiFactory()
-        val response = api.retrieveExistingUser(tokenFactory()).execute().body()?.get(0)
 
-        response!!.userHeadline = "Edited Test"
-        val addToDatabase = api.updateExistingUser(tokenFactory(), response?.userId.toString(), response).execute()
-        val putRequestCode = addToDatabase.code()
-        assertEquals(200, putRequestCode)
-        println(addToDatabase)
+        // get user to start off
+        // store their user data for cross-checking at the end of the test
+        val originalUser = api.retrieveExistingUser(tokenFactory()).execute().body()!![0]
+
+
+        // change user info (where it's allowed) and update, making sure the request succeeds
+        var originalTestUser = originalUser.copy()
+        originalTestUser.userHeadline = "Original Test Headline"
+        originalTestUser.userName = "Original Test Username"
+        assertEquals(200, api.updateExistingUser(tokenFactory(), originalTestUser.userId.toString(),originalTestUser).execute().code())
+
+        // get the user again
+        originalTestUser = api.retrieveExistingUser(tokenFactory()).execute().body()!![0]
+
+        // make sure the username and headline are the original test strings
+        assertEquals(originalTestUser.userHeadline, "Original Test Headline")
+        assertEquals(originalTestUser.userName, "Original Test Username")
+
+        // make sure they're truly different from the original user
+        assertNotEquals(originalUser.userHeadline, originalTestUser.userHeadline)
+        assertNotEquals(originalUser.userName, originalTestUser.userName)
+
+        // make sure none of the fields that SHOULD NOT BE CHANGEABLE, have not changed
+        assertEquals(originalTestUser.firebaseUID, originalUser.firebaseUID)
+        assertEquals(originalTestUser.userId, originalUser.userId)
+
+        // for good measure
+        // change user info (where it's allowed) and update, making sure the request succeeds
+        var editedTestUser = originalUser.copy()
+        editedTestUser.userHeadline = "Edited Test Headline"
+        editedTestUser.userName = "Edited Test Username"
+        assertEquals(200, api.updateExistingUser(tokenFactory(), editedTestUser.userId.toString(),editedTestUser).execute().code())
+
+        // get the user again
+        editedTestUser = api.retrieveExistingUser(tokenFactory()).execute().body()!![0]
+
+        // make sure the username and headline are the original test strings
+        assertEquals(editedTestUser.userHeadline, "Edited Test Headline")
+        assertEquals(editedTestUser.userName, "Edited Test Username")
+
+        // make sure they're truly different from the original user
+        assertNotEquals(editedTestUser.userHeadline, originalTestUser.userHeadline)
+        assertNotEquals(editedTestUser.userName, originalTestUser.userName)
+
+        // make sure none of the fields that SHOULD NOT BE CHANGEABLE, have not changed
+        assertEquals(editedTestUser.firebaseUID, originalUser.firebaseUID)
+        assertEquals(editedTestUser.userId, originalUser.userId)
+
+        // put back the original user info, from before the test was ran, making sure the update succeeds.
+        assertEquals(200, api.updateExistingUser(tokenFactory(), originalUser.userId.toString(),originalUser).execute().code())
     }
     // GET Api response tests
     // returns 200 for successful GET from the Api
