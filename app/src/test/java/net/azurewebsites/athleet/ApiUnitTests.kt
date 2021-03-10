32a7d9abe280c7961cb70c97fbea85e953a0abb6
tpaladichuk <taylor.paladichuk@oit.edu>
2021-03-09 22:36:01 -0800

    Finished unit tests for updating a User's info. The checkEditUser thoroughly ensures that the only the Username and Headline are modified in the process of performing multiple changes. I've included print messages in the debugger to make it easier to identify each step.

--- app/src/test/java/net/azurewebsites/athleet/ApiUnitTests.kt
+++ app/src/test/java/net/azurewebsites/athleet/ApiUnitTests.kt
@@ -43,57 +43,66 @@ class ApiUnitTests {
     @Test
     fun checkEditUser() {
         val api = apiFactory()
+        println("\nBEGIN EDIT USER TEST\n")
+        println("\nGetting the test user's current account information for restoring after test completes...\n")
 
         // get user to start off
         // store their user data for cross-checking at the end of the test
-        val originalUser = api.retrieveExistingUser(tokenFactory()).execute().body()!![0]
+        val originalUser = api.retrieveExistingUser(tokenFactory()).execute().body()!![0];  println("\nOriginal Username: ${originalUser.userName}\nOriginal Headline: ${originalUser.userHeadline}\n")
 
 
         // change user info (where it's allowed) and update, making sure the request succeeds
-        var originalTestUser = originalUser.copy()
-        originalTestUser.userHeadline = "Original Test Headline"
-        originalTestUser.userName = "Original Test Username"
-        assertEquals(200, api.updateExistingUser(tokenFactory(), originalTestUser.userId.toString(),originalTestUser).execute().code())
+        val originalTestUser = originalUser.copy(); println("\nBeginning first edit to user data...\n")
+        originalTestUser.userHeadline = "First Test Headline";
+        originalTestUser.userName = "First Test Username";  println("\nSending PUT request with API call updateExistingUser...\n")
+
+        // make the API call and make sure it succeeds
+        assertEquals(200, api.updateExistingUser(tokenFactory(), originalTestUser.userId.toString(),originalTestUser).execute().code());    println("\nFirst PUT request succeeded.\n");println("\nBeginning GET request with API call to retrieveExistingUser...\n")
 
         // get the user again
-        originalTestUser = api.retrieveExistingUser(tokenFactory()).execute().body()!![0]
+        val originalTestUserReturn = api.retrieveExistingUser(tokenFactory()).execute().body()!![0];    println("\nFirst Edited Username: ${originalTestUserReturn.userName}\nFirst Edited Headline: ${originalTestUserReturn.userHeadline}\n")
 
         // make sure the username and headline are the original test strings
-        assertEquals(originalTestUser.userHeadline, "Original Test Headline")
-        assertEquals(originalTestUser.userName, "Original Test Username")
-
-        // make sure they're truly different from the original user
-        assertNotEquals(originalUser.userHeadline, originalTestUser.userHeadline)
-        assertNotEquals(originalUser.userName, originalTestUser.userName)
+        assertEquals(originalTestUserReturn.userHeadline, "First Test Headline")
+        assertEquals(originalTestUserReturn.userName, "First Test Username")
 
         // make sure none of the fields that SHOULD NOT BE CHANGEABLE, have not changed
-        assertEquals(originalTestUser.firebaseUID, originalUser.firebaseUID)
-        assertEquals(originalTestUser.userId, originalUser.userId)
+        assertEquals(originalTestUserReturn.firebaseUID, originalUser.firebaseUID)
+        assertEquals(originalTestUserReturn.userId, originalUser.userId)
+
+        // make sure they're truly different from the original user
+        assertNotEquals(originalUser.userHeadline, originalTestUserReturn.userHeadline)
+        assertNotEquals(originalUser.userName, originalTestUserReturn.userName)
+
 
         // for good measure
         // change user info (where it's allowed) and update, making sure the request succeeds
-        var editedTestUser = originalUser.copy()
-        editedTestUser.userHeadline = "Edited Test Headline"
-        editedTestUser.userName = "Edited Test Username"
-        assertEquals(200, api.updateExistingUser(tokenFactory(), editedTestUser.userId.toString(),editedTestUser).execute().code())
+        val editedTestUser = originalTestUserReturn.copy();     println("\nBeginning second edit to user data...\n")
+        editedTestUser.userHeadline = "Second Test Headline"
+        editedTestUser.userName = "Second Test Username"
+        assertEquals(200, api.updateExistingUser(tokenFactory(), editedTestUser.userId.toString(),editedTestUser).execute().code());println("\nSecond PUT request succeeded.\n");println("\nBeginning GET request with API call to retrieveExistingUser...\n")
 
         // get the user again
-        editedTestUser = api.retrieveExistingUser(tokenFactory()).execute().body()!![0]
-
+        val editedTestUserReturn = api.retrieveExistingUser(tokenFactory()).execute().body()!![0];
+        println("\nSecond Edited Username: ${editedTestUserReturn.userName}\nSecond Edited Headline: ${editedTestUserReturn.userHeadline}\n")
         // make sure the username and headline are the original test strings
-        assertEquals(editedTestUser.userHeadline, "Edited Test Headline")
-        assertEquals(editedTestUser.userName, "Edited Test Username")
+        assertEquals(editedTestUserReturn.userHeadline, "Second Test Headline")
+        assertEquals(editedTestUserReturn.userName, "Second Test Username")
 
         // make sure they're truly different from the original user
-        assertNotEquals(editedTestUser.userHeadline, originalTestUser.userHeadline)
-        assertNotEquals(editedTestUser.userName, originalTestUser.userName)
+        assertNotEquals(editedTestUserReturn.userHeadline, originalTestUserReturn.userHeadline)
+        assertNotEquals(editedTestUserReturn.userName, originalTestUserReturn.userName)
+        assertNotEquals(editedTestUserReturn.userHeadline, originalUser.userHeadline)
+        assertNotEquals(editedTestUserReturn.userName, originalUser.userName)
 
         // make sure none of the fields that SHOULD NOT BE CHANGEABLE, have not changed
         assertEquals(editedTestUser.firebaseUID, originalUser.firebaseUID)
         assertEquals(editedTestUser.userId, originalUser.userId)
 
+        println("\nOriginal Username: ${originalUser.userName}\n\t and \n Original Headline: ${originalUser.userHeadline} \n\t will now be returned to the database.\n")
         // put back the original user info, from before the test was ran, making sure the update succeeds.
         assertEquals(200, api.updateExistingUser(tokenFactory(), originalUser.userId.toString(),originalUser).execute().code())
+        println("\nEdit test completed successfully.\n")
     }
     // GET Api response tests
     // returns 200 for successful GET from the Api
