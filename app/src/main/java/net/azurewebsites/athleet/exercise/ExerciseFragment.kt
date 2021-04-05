ef7e074ebf5228fcff8ccc3bef890e96e3699574
ntout <nathan.tout@oit.edu>
2021-04-05 00:10:58 -0700

    63: added UI prompts for empty fields and buttons

--- app/src/main/java/net/azurewebsites/athleet/exercise/ExerciseFragment.kt
+++ app/src/main/java/net/azurewebsites/athleet/exercise/ExerciseFragment.kt
@@ -1,14 +1,17 @@
 package net.azurewebsites.athleet.exercise
 
+import android.R.attr.button
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import net.azurewebsites.athleet.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.fragment_exercise.*
 import net.azurewebsites.athleet.databinding.FragmentExerciseBinding
 import net.azurewebsites.athleet.models.Exercise
 
@@ -28,14 +31,132 @@ class ExerciseFragment : Fragment() {
         val exercise: Exercise =  ExerciseFragmentArgs.fromBundle(requireArguments()).selectedExercise
         val viewModelFactory = ExerciseViewModelFactory(exercise, application)
         binding.viewModel = ViewModelProvider(
-            this, viewModelFactory).get(ExerciseViewModel::class.java)
+            this, viewModelFactory
+        ).get(ExerciseViewModel::class.java)
 
+        var edit = false
+        isEditing(edit, binding)
 
-        val items = listOf("lbs", "km", "mi", "¥", "psi", "kCal", "Android")
-        val adapter = ArrayAdapter(requireContext(), R.layout.measureunit_list_item, items)
-        binding.exerciseDetailUnit.setAdapter(adapter)
+        binding.fabEditExercise.setOnClickListener {
+            edit = true
+            isEditing(edit, binding)
+            binding.btnCancelEdit.visibility = View.VISIBLE
+            binding.btnSaveEdit.visibility = View.VISIBLE
+            binding.fabEditExercise.visibility = View.GONE
+
+            markFields(binding)
+        }
+
+        binding.btnCancelEdit.setOnClickListener {
+            unmarkAllFields(binding)
+            binding.btnCancelEdit.visibility = View.GONE
+            binding.btnSaveEdit.visibility = View.GONE
+            binding.fabEditExercise.visibility = View.VISIBLE
+
+            edit = false
+            isEditing(edit, binding)
+        }
+
+        binding.btnSaveEdit.setOnClickListener {
+            unmarkAllFields(binding)
+            // TODO: Update the view model with the new values
+            // TODO: Send the updated exercise to the DB with the updateExercise() api method
+            binding.btnCancelEdit.visibility = View.GONE
+            binding.btnSaveEdit.visibility = View.GONE
+            binding.fabEditExercise.visibility = View.VISIBLE
+
+            edit = false
+            isEditing(edit, binding)
+        }
+
+        binding.btnSaveEdit.isEnabled = false
+        val editTexts = listOf(
+            binding.exerciseDetailSets,
+            binding.exerciseDetailName,
+            binding.exerciseDetailDesc,
+            binding.exerciseDetailReps,
+            binding.exerciseDetailUnit,
+            binding.exerciseUnitCount
+        )
+
+        for (editText in editTexts) {
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    var et1 = binding.exerciseDetailSets.text.toString().trim()
+                    var et2 = binding.exerciseDetailName.text.toString().trim()
+                    var et3 = binding.exerciseDetailDesc.text.toString().trim()
+                    var et4 = binding.exerciseDetailReps.text.toString().trim()
+                    var et5 = binding.exerciseDetailUnit.text.toString().trim()
+                    var et6 = binding.exerciseUnitCount.text.toString().trim()
+
+                    binding.btnSaveEdit.isEnabled = et1.isNotEmpty()
+                            && et2.isNotEmpty()
+                            && et3.isNotEmpty()
+                            && et4.isNotEmpty()
+                            && et5.isNotEmpty()
+                            && et6.isNotEmpty()
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence, start: Int, count: Int, after: Int) {
+                }
+
+                override fun afterTextChanged(
+                    s: Editable) {
+                    checkValues(editText)
+                }
+            })
+        }
 
         return binding.root
+    }
+
+    private fun isEditing(edit: Boolean, binding: FragmentExerciseBinding) {
+        if (!edit) {
+            binding.exerciseDetailSets.isEnabled = false
+            binding.exerciseDetailName.isEnabled = false
+            binding.exerciseDetailDesc.isEnabled = false
+            binding.exerciseDetailReps.isEnabled = false
+            binding.exerciseDetailUnit.isEnabled = false
+            binding.exerciseUnitCount.isEnabled = false
+        }
+        else {
+            binding.exerciseDetailSets.isEnabled = true
+            binding.exerciseDetailName.isEnabled = true
+            binding.exerciseDetailDesc.isEnabled = true
+            binding.exerciseDetailReps.isEnabled = true
+            binding.exerciseDetailUnit.isEnabled = true
+            binding.exerciseUnitCount.isEnabled = true
+        }
+    }
+
+
+    private fun checkValues(view: TextInputEditText) {
+        val textfield: TextInputEditText = view
+        val containingLayout = textfield.parent as ViewGroup
+        val textInputLayout = containingLayout.parent as TextInputLayout
+
+        if (textfield.text.isNullOrEmpty()){
+            textInputLayout.error = "Required field"
+        } else textInputLayout.setError(null)
+    }
+
+    private fun markFields(binding: FragmentExerciseBinding){
+        checkValues(binding.exerciseDetailSets)
+        checkValues(binding.exerciseDetailName)
+        checkValues(binding.exerciseDetailDesc)
+        checkValues(binding.exerciseDetailReps)
+        checkValues(binding.exerciseDetailUnit)
+        checkValues(binding.exerciseUnitCount)
+    }
+
+    private fun unmarkAllFields(binding: FragmentExerciseBinding) {
+        binding.tilDescription.setError(null)
+        binding.tilUnitName.setError(null)
+        binding.tilName.setError(null)
+        binding.tilQuantity.setError(null)
+        binding.tilReps.setError(null)
+        binding.tilSets.setError(null)
     }
 
 }
