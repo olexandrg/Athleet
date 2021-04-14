ff10b366a296ba3e51a9e1e1969fed0bf1c823c2
Olexandr Goloshchapov <olexandr.goloshchapov@oit.edu>
2021-04-13 18:29:31 -0700

    Initial commit with node modules.

---
+++ app/src/main/java/net/azurewebsites/athleet/MessagingActivity.kt
@@ -0,0 +1,25 @@
+package net.azurewebsites.athleet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.socket.client.Socket
+import io.socket.client.IO;
+import java.net.URISyntaxException
+
+class MessagingActivity : AppCompatActivity() {
+    private var mSocket: Socket? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messaging)
+        try {
+            mSocket = IO.socket("10.0.2.2:3000")
+            mSocket!!.connect()
+        }
+        catch (e: URISyntaxException)
+        {
+        //do nothing
+        }
+    }
+}
