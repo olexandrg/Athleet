e891df481afcf46d7a2b742cba70cb6c37393134
tpaladichuk <taylor.paladichuk@oit.edu>
2021-04-18 19:24:53 -0700

    Added onClick listeners for the Send Message and Block User buttons. For now they only create a toast indicating what will happen once everything is merged.

--- app/src/main/java/net/azurewebsites/athleet/user/OtherUserProfilePageActivity.kt
+++ app/src/main/java/net/azurewebsites/athleet/user/OtherUserProfilePageActivity.kt
@@ -2,6 +2,8 @@ package net.azurewebsites.athleet.user
 
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
@@ -11,22 +13,23 @@ import net.azurewebsites.athleet.models.UserItem
 
 @RequiresApi(Build.VERSION_CODES.O)
 class OtherUserProfilePageActivity : AppCompatActivity() {
-    // store user data locally
     val api = Api.createSafe()
     private lateinit var user: UserItem
     private lateinit var teamMemberUsername : String
     private lateinit var usernameField:TextInputEditText
     private lateinit var headlineField:TextInputEditText
-
+    private lateinit var sendMessageButton:Button
+    private lateinit var blockUserButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.other_user_detail_page)
-        // populate menu with retrieved user data
-        // getUserMenuData()
-        // setupEnterKeyListeners()
         teamMemberUsername = intent!!.extras?.get("userName").toString()
-        usernameField = findViewById<TextInputEditText>(R.id.userName)
-        headlineField = findViewById<TextInputEditText>(R.id.userHeadline)
+        usernameField = findViewById(R.id.userName)
+        headlineField = findViewById(R.id.userHeadline)
+        sendMessageButton = findViewById(R.id.send_message_button)
+        sendMessageButton.setOnClickListener{ Toast.makeText(this, "Opening direct message room...", Toast.LENGTH_SHORT).show() }
+        blockUserButton = findViewById(R.id.block_user_button)
+        blockUserButton.setOnClickListener{ Toast.makeText(this, "Blocking user...", Toast.LENGTH_SHORT).show() }
         usernameField.setText(teamMemberUsername)
         headlineField.setText("< This will be the user's headline once we implement API calls to make them retrievable >")
 
