80800858eef79089367b42684a9635b797327655
tpaladichuk <taylor.paladichuk@oit.edu>
2021-04-19 00:56:03 -0700

    Added a recycler view to UserProfilePageActivity.kt to hold a list of blocked users. Note: this commit is only partially complete, I still need to make a VM to use for the recycler view.

---
+++ app/src/main/java/net/azurewebsites/athleet/user/BlockedUserListAdapter.kt
@@ -0,0 +1,53 @@
+package net.azurewebsites.athleet.user
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import net.azurewebsites.athleet.R
+import net.azurewebsites.athleet.blockedUsersList
+
+class BlockedUserListAdapter(private val onClick: (String) -> Unit) :
+    ListAdapter<String, BlockedUserListAdapter.BlockedUserViewHolder>(BlockedUserDiffCallback) {
+
+    /* ViewHolder for BlockedUser, takes in the inflated view and the onClick behavior. */
+    class BlockedUserViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        private val BlockedUserNameTextView: TextView = itemView.findViewById(R.id.blocked_users_list_username_textView)
+        private var BlockedUserUnblockButton = itemView.findViewById<Button>(R.id.blocked_users_list_unblock_button)
+        init { BlockedUserUnblockButton.setOnClickListener { blockedUsersList.remove(BlockedUserNameTextView.text); };  }
+
+        /* Bind BlockedUser name and image. */
+        fun bind(BlockedUser: String) {
+            BlockedUserNameTextView.text = BlockedUser
+            BlockedUserUnblockButton.setOnClickListener { blockedUsersList.remove(BlockedUser) }
+        }
+    }
+
+    /* Creates and inflates view and return BlockedUserViewHolder. */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlockedUserListAdapter.BlockedUserViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.blocked_user_list_item, parent, false)
+        return BlockedUserViewHolder(view)
+    }
+
+    /* Gets current BlockedUser and uses it to bind view. */
+    override fun onBindViewHolder(holder: BlockedUserViewHolder, position: Int) {
+        val BlockedUser = getItem(position)
+        holder.bind(BlockedUser)
+    }
+}
+
+object BlockedUserDiffCallback : DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+}
